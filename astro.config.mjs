b107e7581f5163bcba990d7eed59880d9173f068
aleleo1 <100156426+aleleo1@users.noreply.github.com>
2024-02-12 11:46:06 +0100

    added prefetch & minor fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   output: "server",
   adapter: vercel({ imageService: true })
 });
