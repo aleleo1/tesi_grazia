046bdaf09ebcfa9aa3d9e8018e0bbe4291b93308
aleleo1 <100156426+aleleo1@users.noreply.github.com>
2024-02-16 12:06:24 +0100

    added tailwind + indice

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   output: "server",
-  adapter: vercel({ imageService: true })
+  adapter: vercel({
+    imageService: true
+  }),
+  integrations: []
 });
